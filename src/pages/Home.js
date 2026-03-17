bd8ee4b3886d9026bd8c5a7aa871d32ccbb9adac
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:18:59 +0100

    JAVAS: Add src/pages/Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+```javascript
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to Fintech Startup</h1>
+            <p>This is the home page.</p>
+        </div>
+    );
+}
+
+export default Home;
+```
+
+###
