7ca12794c3b2b7667a77ca3347ed5e54a90729b9
emzamike1997-lab <emzamike1997@gmail.com>
2026-03-17 12:18:58 +0100

    JAVAS: Add src/App.js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+```javascript
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+
+function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
+```
+
+###
